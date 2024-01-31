5b367aaeed48198f70615db43dde947f2ec2229f
ashmitadhakal <adhakal2@wisc.edu>
2024-01-31 08:22:31 -0600

    Updates in JS and CSS files

--- Chapter01/js/main.js
+++ Chapter01/js/main.js
@@ -1,1 +1,5 @@
 /* Scripts by Ashmita , 2024 */
+var mydiv = document.getElementById("mydiv");
+            mydiv.addEventListener("click", function(){
+                 alert("Hello World!");
+            });
